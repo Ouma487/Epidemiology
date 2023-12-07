deab0a2bcb3bc1650d734c25d6b3c3cee7891d83
Oumac-137 <94168523+Ouma487@users.noreply.github.com>
2023-12-07 17:53:00 +0100

    Add files via upload

---
+++ epidemilogie/test_visualisation.py
@@ -0,0 +1,9 @@
+from visualisation import *
+
+def test_visualisation():
+    A=[[0,0,1,2,3],[0,1,3,2,0],[0,1,0,1,3]] #test avec cette matrice
+    print(visualisation(A))
+
+
+
+print(test_visualisation())
